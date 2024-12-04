51818df4ccfb3afb65af8e849935f527929d3658
nikita <nikita.goutor@gmail.com>
2024-12-04 16:21:01 +0300

    Update.

---
+++ src/main/java/com/microservices/order/service/OrderService.java
@@ -0,0 +1,37 @@
+package com.microservices.order.service;
+
+import com.microservices.order.client.InventoryClient;
+import com.microservices.order.dto.OrderRequest;
+import com.microservices.order.model.Order;
+import com.microservices.order.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if (isProductInStock) {
+
+            // map OrderRequest to Order object
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+
+            //save order to OrderRepository
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with sku code " + orderRequest.skuCode() + " is not in stock");
+        }
+    }
+}
